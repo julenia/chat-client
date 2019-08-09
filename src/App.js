7c9290baf9a61322399a72cc6cda20fe50474230
julenia <julia.jankowska@onet.eu>
2019-08-09 12:30:48 +0200

    messages displayed for all the users (old and new ones)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import './App.css';
+import * as request from 'superagent'
 
-class App extends React.Component{
-    state = {
-      messages: []
-    }
+class App extends React.Component {
+  state = {
+    message: "",
+    messages: []
+  }
   source = new EventSource(`http://localhost:5000/stream`)
 
-  componentDidMount(){
-    this.source.onmessage = (event)=>{
-      const newMessages = [...this.state.messages, event.data]
-      this.setState({messages: newMessages})
+  componentDidMount() {
+    this.source.onmessage = (event) => {
+      const messages = JSON.parse(event.data)
+      this.setState({ messages })
     }
   }
-  render(){
-    const messages = this.state.messages.map((message, i) =><p key={i}>{message}</p>)
-    return <div>{messages}</div>
+  onSubmit =async (event)=> {
+    event.preventDefault()
+    const response = await request
+      .post('http://localhost:5000/message')
+      .send({message: this.state.message})
+    this.setState({message: ''})
+  }
+
+  onChange=(event)=>{
+    const {value} = event.target
+    this.setState({ message: value })
+  }
+
+  render() {
+    const messages = this.state.messages.map((message, i) => <p key={i}>{message}</p>)
+    const form = <form onSubmit={this.onSubmit}>
+      <input type="text" value={this.state.message} onChange={this.onChange}/>
+      <button type="submit">Send</button>
+    </form>
+    return <main>
+      {form}
+      {messages}
+    </main>
   }
 }
 
